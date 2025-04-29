26fc8ec2177842c45fadb28bda4512c60479c7d5
Anu2024-coder <gillanu2003@gmail.com>
2025-04-29 19:39:53 +0530

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My First JavaScript Code</title>
+</head>
+<body>
+  <h1>Hello JavaScript!</h1>
+
+  <script>
+    // This is a comment
+    console.log("Hello from JavaScript!"); // This shows in the browser console
+    document.write("This is my first JavaScript code!"); // This writes directly on the page
+  </script>
+</body>
+</html>
